37609baf7f97b67d487921fb36422ae6ace7a110
JerinDas <jerindauntless@gmail.com>
2026-08-10 16:14:40 +0530

    backend is running

--- server.js
+++ server.js
@@ -40,4 +40,9 @@ app.post("/contact", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+f
+
 app.listen(5000, () => console.log("Server running on port 5000"));
